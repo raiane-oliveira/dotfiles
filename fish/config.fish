3bd19bda82528fbb73b2793d4b2a9d0486b2cca6
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-12-28 20:31:32 -0300

    add fzf themes and update tmux config

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,7 @@ source ~/.asdf/asdf.fish
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # pnpm
-set -gx PNPM_HOME "/home/raiane/.local/share/pnpm"
+set -gx PNPM_HOME "/home/raianeeo/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
@@ -64,7 +64,21 @@ export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git "
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
-export FZF_DEFAULT_OPTS="--height 50% --layout=default --border --color=hl:#ea83a5"
+# FZF Rose pine theme
+# export FZF_DEFAULT_OPTS="--height 50% --layout=default --border \
+# --color=fg:#908caa,bg:#191724,hl:#ebbcba
+# --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+# --color=border:#403d52,header:#31748f,gutter:#191724
+# --color=spinner:#f6c177,info:#9ccfd8
+# --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
+# FZF Mellow theme
+export FZF_DEFAULT_OPTS="--height 50% --layout=default --border \
+--color=fg:#c9c7cd,bg:#161617,hl:#f5a191 \
+--color=fg+:#c1c0d4,bg+:#1b1b1d,hl+:#ffae9f \
+--color=border:#2a2a2d,header:#90b99f,gutter:#161617 \
+--color=spinner:#ecaad6,info:#9dc6ac \
+--color=pointer:#ea83a5,marker:#e29eca,prompt:#e6b99d"
 export FZF_TMUX_OPTS=" -p90%,70% "
 
 # Setup fzf previews
