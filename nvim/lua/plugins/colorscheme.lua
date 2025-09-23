8c26514d8baaa53cdbe803ea9939cf78c1ab868c
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-09-23 20:41:41 -0300

    nvim, ghostty & starship: update configs

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,33 +13,6 @@ local future_themes = {
     end,
   },
   {
-    "mellow-theme/mellow.nvim",
-    priority = 1000,
-    config = function()
-      local c = require("mellow.colors")
-
-      vim.g.mellow_italic_functions = true
-      vim.g.mellow_bold_functions = true
-      vim.g.mellow_transparent = transparency
-      vim.g.mellow_highlight_overrides = {
-
-        -- Diagnostics
-        ["DiagnosticUnderlineError"] = { fg = c.red, underline = true },
-        ["DiagnosticUnderlineWarn"] = { fg = c.yellow, underline = true },
-        ["DiagnosticUnderlineInfo"] = { fg = c.blue, underline = true },
-        ["DiagnosticUnderlineHint"] = { fg = c.cyan, underline = true },
-
-        -- Neovim's built-in language server client
-        ["LspReferenceWrite"] = { fg = c.blue, underline = true },
-        ["LspReferenceText"] = { fg = c.blue, underline = true },
-        ["LspReferenceRead"] = { fg = c.blue, underline = true },
-        ["LspSignatureActiveParameter"] = { fg = c.yellow, bold = true },
-      }
-
-      vim.g.edge_enable_italic = true
-    end,
-  },
-  {
     "everviolet/nvim",
     name = "evergarden",
     priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
@@ -81,7 +54,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "mellifluous",
+      colorscheme = "mellow",
     },
   },
   -- Using lazy.nvim
@@ -94,7 +67,7 @@ return {
         -- optional configuration here
         transparent = transparency,
       })
-      require("black-metal").load()
+      -- require("black-metal").load()
     end,
   },
   {
@@ -102,8 +75,35 @@ return {
     opts = {
       colorset = "mountain",
       transparent_background = {
-        enabled = true,
+        enabled = transparency,
       },
     },
+  },
+  {
+    "mellow-theme/mellow.nvim",
+    priority = 1000,
+    config = function()
+      local c = require("mellow.colors")
+
+      vim.g.mellow_italic_functions = true
+      vim.g.mellow_bold_functions = true
+      vim.g.mellow_transparent = transparency
+      vim.g.mellow_highlight_overrides = {
+
+        -- Diagnostics
+        ["DiagnosticUnderlineError"] = { fg = c.red, underline = true },
+        ["DiagnosticUnderlineWarn"] = { fg = c.yellow, underline = true },
+        ["DiagnosticUnderlineInfo"] = { fg = c.blue, underline = true },
+        ["DiagnosticUnderlineHint"] = { fg = c.cyan, underline = true },
+
+        -- Neovim's built-in language server client
+        ["LspReferenceWrite"] = { fg = c.blue, underline = true },
+        ["LspReferenceText"] = { fg = c.blue, underline = true },
+        ["LspReferenceRead"] = { fg = c.blue, underline = true },
+        ["LspSignatureActiveParameter"] = { fg = c.yellow, bold = true },
+      }
+
+      vim.g.edge_enable_italic = true
+    end,
   },
 }
