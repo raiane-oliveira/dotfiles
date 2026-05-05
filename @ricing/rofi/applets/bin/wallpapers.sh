12a02be5302074615ae68a70c6d37eee0f9bffd9
Raiane Oliveira <raiane.oliveira404@gmail.com>
2026-05-05 15:44:34 -0300

    feat(rofi): add wallpapers applet, hypr keybind; tweak style & walset

---
+++ @ricing/rofi/applets/bin/wallpapers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## Author  : Raiane Oliveira
+## Github  : @raiane-oliveira
+#
+## Applets : Change wallpaper (swww)
+
+# Import Current Theme
+source "$HOME"/.config/rofi/applets/shared/theme.bash
+theme="$type/$style"
+
+# Directory where wallpapers are stored
+WALL_DIR="$HOME/Pictures/Wallpapers"
+
+# Current directory (to cd back to)
+CMD="$(pwd)"
+
+cd $WALL_DIR || exit
+
+# Handle spaces in filenames
+IFS=$'\n'
+
+# Grab the user-selected wallpaper using rofi
+SELECTED_WALL=$(for a in *.jpg *.png *.jpeg; do echo -en "$a\0icon\x1f$a\n"; done | rofi \
+  -dmenu \
+  -theme "$theme" \
+  -show-icons \
+  -p "󰥷" \
+  -theme-str 'window { width: 1000px; height: 500px; }' \
+  -theme-str 'mainbox { orientation: horizontal; children: [ "left-pane", "right-box" ]; spacing: 0px; }' \
+  -theme-str 'left-pane { width: 600px; expand: false; children: [ "icon-current-entry" ]; background-color: transparent; }' \
+  -theme-str 'icon-current-entry { size: 600px; horizontal-align: 0.5; vertical-align: 0.25; background-color: transparent; }' \
+  -theme-str 'right-box { orientation: vertical; children: [ "inputbar", "listview" ]; expand: true; }' \
+  -theme-str 'inputbar { children: [ "prompt", "entry" ]; spacing: 10px; padding: 15px 10px; background-color: transparent; }' \
+  -theme-str 'entry { placeholder: "Pesquisar..."; cursor: text; text-color: @foreground; background-color: @background-alt; padding: 10px 14px; }' \
+  -theme-str 'listview { columns: 1; lines: 10; expand: true; padding: 10px; }' \
+  -theme-str 'element { orientation: horizontal; padding: 8px; spacing: 10px; }' \
+  -theme-str 'element-icon { size: 48px; background-color: transparent; border: 0px; }' \
+  -theme-str 'textbox-prompt-colon { str: ":"; }' \
+  -theme-str 'prompt { background-color: @urgent; padding: 10px 14px; }')
+
+# If not empty, pass to walset
+if [ -n "$SELECTED_WALL" ]; then
+  walset "$SELECTED_WALL"
+fi
+
+# Go back to where you came from
+cd "$CMD" || exit
--- scripts/walset.sh
+++ scripts/walset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awww img "$HOME/Pictures/Wallpapers/$1" --transition-type=center --transition-duration=0.8 --transition-fps=60
+awww img "$1" --transition-type=center --transition-duration=0.8 --transition-fps=60
