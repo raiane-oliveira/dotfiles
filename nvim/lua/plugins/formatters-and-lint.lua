03d5c82e66e0352dbd158a0200578a83bbed0d59
Raiane Oliveira <raiane.oliveira404@gmail.com>
2026-08-15 03:02:19 -0300

    feat(fish): add copilot plugins completions and update configs

--- nvim/lua/plugins/formatters-and-lint.lua
+++ nvim/lua/plugins/formatters-and-lint.lua
@@ -1,12 +1,28 @@
 local formatJSFiles = function(bufnr)
   local conform = require("conform")
-  if conform.get_formatter_info("eslint_d", bufnr).available then
-    return { "eslint_d" }
-  elseif conform.get_formatter_config("biome", bufnr) then
-    return { "biome" }
-  else
-    return { "prettierd" }
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+  local function eslint_d_ok()
+    local info = conform.get_formatter_info("eslint_d", bufnr)
+    if not info.available then
+      return false
+    end
+    -- `available` só confirma que o binário existe e que HÁ um arquivo
+    -- de config por perto -- não que ele é válido. Testamos de verdade:
+    vim.fn.system({ "eslint_d", "--print-config", bufname })
+    return vim.v.shell_error == 0
   end
+
+  if eslint_d_ok() then
+    return { "eslint_d" }
+  end
+
+  local biome_info = conform.get_formatter_info("biome", bufnr)
+  if biome_info.available then
+    return { "biome" }
+  end
+
+  return { "prettierd" }
 end
 
 return {
